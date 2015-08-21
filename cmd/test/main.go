15a05023bc139e656798269011f0009b9ee688ca
Eric J. Holmes <eric@ejholmes.net>
2015-08-21 14:49:03 -0700

    Cleanup.

---
+++ cmd/test/main.go
@@ -0,0 +1,36 @@
+// Just a small CLI application that writes to Stdout whatever it reads from
+// Stdin, using a database stream.
+package main
+
+import (
+	"database/sql"
+	"io"
+	"log"
+	"os"
+
+	"github.com/ejholmes/logstream"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db, err := sql.Open("postgres", "postgres://localhost/logstream?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rw := logstream.New("abcd", db)
+
+	go func() {
+		if _, err := io.Copy(rw, os.Stdin); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := rw.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	if _, err := io.Copy(os.Stdout, rw); err != nil {
+		log.Fatal(err)
+	}
+}
